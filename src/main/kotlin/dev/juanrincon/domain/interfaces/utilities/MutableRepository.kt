9ea4e62ed51999bd2f3499106cfe354d783af7b2
Juan Rincon <justanotherone17727@gmail.com>
2022-09-18 18:45:06 -0600

    Resource Repository Interface

--- src/main/kotlin/dev/juanrincon/domain/interfaces/utilities/MutableRepository.kt
+++ src/main/kotlin/dev/juanrincon/domain/interfaces/utilities/MutableRepository.kt
@@ -1,10 +1,10 @@
 package dev.juanrincon.domain.interfaces.utilities
 
-interface MutableRepository<M, E> {
+interface MutableRepository<I, O> {
 
     suspend fun delete(id: Int): Boolean
 
-    suspend fun add(entry: M): E
+    suspend fun add(entry: I): O
 
-    suspend fun  update(id: Int, entry: M): E?
+    suspend fun  update(id: Int, entry: I): O?
 }
