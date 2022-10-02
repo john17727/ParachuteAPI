10b8b0c96741bff144cf376cf1bc2fa06dc6a690
Juan Rincon <justanotherone17727@gmail.com>
2022-10-02 12:15:26 -0600

    Utility Repository

--- src/main/kotlin/dev/juanrincon/domain/daos/ProjectStatusEntity.kt
+++ src/main/kotlin/dev/juanrincon/domain/daos/ProjectStatusEntity.kt
@@ -1,10 +1,17 @@
 package dev.juanrincon.domain.daos
 
+import dev.juanrincon.domain.interfaces.utilities.DomainMapper
+import dev.juanrincon.domain.models.GenericType
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
-class ProjectStatusEntity(id: EntityID<Int>): IntEntity(id) {
+class ProjectStatusEntity(id: EntityID<Int>): IntEntity(id), DomainMapper<GenericType> {
     companion object : IntEntityClass<ProjectStatusEntity>(ProjectStatusTable)
     var name by ProjectStatusTable.name
+
+    override fun toModel() = GenericType(
+        id.value,
+        name
+    )
 }
